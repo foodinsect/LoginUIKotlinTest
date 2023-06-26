6b36ba140818b998d43962a8d5bb3ee4dfe5a8bd
Minjin <opqrs0422@naver.com>
2023-06-26 17:01:29 +0900

    코드에 적은 관리자 이름도 변수에 나오게  - String 가공하기

--- app/src/main/java/com/neppplus/loginuikotlintest/MainActivity.kt
+++ app/src/main/java/com/neppplus/loginuikotlintest/MainActivity.kt
@@ -22,8 +22,9 @@ class MainActivity : AppCompatActivity() {
             // 두개의 변수에 들어있는 값 검사. (맞는 아이디/비번)
             // 맞을 때 => 틀릴 때 분기 처리. (if 활용)
             if (inputId == "admin@test.com" && inputPw == "qwer"){
-                //둘 다 맞다면 => 관리자 입니다. 토스트
-                Toast.makeText(this, "관리자입니다.", Toast.LENGTH_SHORT).show()
+                //둘 다 맞다면 => 장민진 관리자 입니다. 토스트
+                val name = "장민진"
+                Toast.makeText(this, "${name} 관리자입니다.", Toast.LENGTH_SHORT).show()
             }
             else{
                 //아이디, 비번 둘 중 하나라도 틀리면 로그인 실패 토스트
